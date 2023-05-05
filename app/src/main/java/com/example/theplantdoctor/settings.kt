754e11a809423a79cf433ddf18c8ec1e01a05466
Clauvet1 <mongambaclauvet@gmail.com>
2023-05-05 16:35:35 +0100

    Settings page

---
+++ app/src/main/java/com/example/theplantdoctor/settings.kt
@@ -0,0 +1,11 @@
+package com.example.theplantdoctor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
